f92c9ead72549ecdef62226dd118d90a50860b18
Michael Anckaert <michael.anckaert@sinax.be>
2021-11-04 14:16:06 +0100

    Add actual python test

--- src/tests.py
+++ src/tests.py
@@ -1,9 +1,15 @@
-from unittest import TestCase
+import unittest
 
 from greeter import Greeter
 
 
-class GreeterTest(TestCase):
+class GreeterTest(unittest.TestCase):
     def test_create_greeting(self):
         result = Greeter.create_greeting()
         self.assertEqual(result, "Hello, world!")
+
+    def test_fail(self):
+        self.assertEqual(2, 3)
+
+if __name__ == "__main__":
+    unittest.main()
